c1bb93f5b20c799573efc527142212716a1e304e
medilore <fc2tokaqq+medilore@gmail.com>
2023-01-15 07:02:36 +0900

    init commit

---
+++ src/redis/RedisClient.ts
@@ -0,0 +1,15 @@
+import { createClient } from "redis";
+
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const REDIS_USER = process.env.REDIS_USER;
+const REDIS_PASSWORD = process.env.REDIS_PASSWORD;
+const REDIS_HOST = process.env.REDIS_HOST;
+const REDIS_PORT = process.env.REDIS_PORT;
+
+const client = createClient({
+    url: `redis://${REDIS_USER}:${REDIS_PASSWORD}@${REDIS_HOST}:${REDIS_PORT}`,
+});
+
+export const RedisClient = client;
