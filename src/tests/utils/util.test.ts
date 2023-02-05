ed18fe1a36cf5be7f87c135ad1f9c909eaccc45e
sev3e3e <fc2tokaqq+medilore@gmail.com>
2023-02-05 15:48:34 +0900

    謎テスト削除

--- src/tests/utils/util.test.ts
+++
@@ -1,7 +0,0 @@
-import { ReduceCacheExpiration } from "../../utils/util";
-
-describe("Util関数群のテスト", () => {
-    test("ReduceCacheExpiration", () => {
-        expect(ReduceCacheExpiration(1000)).toEqual(700);
-    });
-});
